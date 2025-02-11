97f2e31250d059271cc4337c40d154ae8edfdaf2
saikrishna76845 <165982460+saikrishna76845@users.noreply.github.com>
2025-02-11 17:42:34 +0530

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { MatDialog} from '@angular/material/dialog';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { TemplateRef } from '@angular/core';
 import { EmployeeService } from '../services/employee.service';
-
+import { Router } from '@angular/router';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
@@ -46,7 +46,7 @@ export class HomeComponent implements OnInit, AfterViewInit  {
 
   displayedColumns: string[] = ['employeeId', 'name', 'email', 'phone', 'actions'];
 
-  constructor( private dialog: MatDialog, private fb: FormBuilder, private employeeService: EmployeeService) {
+  constructor( private dialog: MatDialog, private fb: FormBuilder, private employeeService: EmployeeService, private router: Router) {
     const firebaseApp = initializeApp(environment.firebase);
     
     // console.log('Firebase initialized:', firebaseApp);
@@ -67,16 +67,7 @@ export class HomeComponent implements OnInit, AfterViewInit  {
   }
 
   ngAfterViewInit(): void {
-    this.dialogTemplateAvailable = !!this.dialogTemplate;
-  }
-
-  checkPhoneExists(phone: string) {
-    const employeesRef = collection(this.firestore, 'Employees');
-    const q = query(employeesRef, where("phone", "==", phone));
-  
-    return getDocs(q).then(querySnapshot => {
-      return querySnapshot.empty;  // Returns true if no document with the same phone exists
-    });
+    this.dialogTemplateAvailable = !!this.dialogTemplate; //!! this express boolean
   }
 
   async fetchEmployees(): Promise<void> {
@@ -87,125 +78,26 @@ export class HomeComponent implements OnInit, AfterViewInit  {
     }
   }
 
-  openDialog(employee: Employee | null = null): void {
-    console.log('Opening dialog for employee:', employee);
-    if (employee) {
-      this.employeeForm.patchValue({
-        employeeId: employee.employeeId +1,
-        name: employee.name,
-        email: employee.email,
-        phone: employee.phone,
-      });
-    } else {
-      this.employeeForm.reset();
-    }
-
+  openDialog(): void {
+    this.employeeForm.reset()
     const dialogRef = this.dialog.open(this.dialogTemplate!, {
       width: '2000px',
     });
 
-    dialogRef.afterClosed().subscribe((updatedEmployee: Employee) => {
-      // console.log('Dialog closed. Updated employee:', updatedEmployee);
-      
-      if (updatedEmployee) {
-        this.onEditSave(updatedEmployee); 
-      } else {
-          this.onSave(); 
-        }
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.onSave(); 
+      } 
     });
   }
 
-  deleteEmployee(employeeId: number): void {
+  checkPhoneExists(phone: string) {
     const employeesRef = collection(this.firestore, 'Employees');
-    
-    const q = query(employeesRef, where("employeeId", "==", employeeId));
-
-    getDocs(q)
-      .then((querySnapshot) => {
-        querySnapshot.forEach((docSnapshot) => {
-          console.log('Deleting document with ID:', docSnapshot.id);
-          deleteDoc(docSnapshot.ref)
-            .then(() => {
-              console.log('Employee deleted from Firestore');
-              this.fetchEmployees();
-              // alert('Employee deleted successfully!');
-            })
-            .catch((error) => {
-              console.error('Error deleting employee from Firestore: ', error);
-              alert('Error deleting employee. Please try again.');
-            });
-        });
+    const q = query(employeesRef, where("phone", "==", phone));
   
-        
-        if (querySnapshot.empty) {
-          alert('No employee found with this ID.');
-        }
-      })
-      .catch((error) => {
-        console.error('Error finding employee by employeeId: ', error);
-        alert('Error finding employee. Please try again.');
-      });
-  }
-  
-
-  editEmployee(employeeId: number): void {
-    console.log('Opening dialog to edit employee:', employeeId);
-    const employee = this.employees.find(emp => emp.employeeId === employeeId);
-
-  if (employee) {
-    this.employeeForm.patchValue({
-      employeeId: employee.employeeId,
-      name: employee.name,
-      email: employee.email,
-      phone: employee.phone,
+    return getDocs(q).then(querySnapshot => {
+      return querySnapshot.empty;  // Returns true if no document with the same phone exists
     });
-    
-    const dialogRef = this.dialog.open(this.dialogTemplate!, {
-      width: '2000px',
-      data: employee,  
-    });
-
-    dialogRef.afterClosed().subscribe((updatedEmployee: Employee) => {
-      if (updatedEmployee) {
-        this.onEditSave(updatedEmployee);
-      } else {
-        console.log('No updated employee data received');
-      }
-    });
-  } else {
-    console.log('Employee not found with ID:', employeeId);
-  }
-  }
-
-  onEditSave(updatedEmployee: Employee): void {
-    console.log('Saving updated employee:', updatedEmployee);
-    if (this.employeeForm.valid) {
-      const phone = this.employeeForm.value.phone;
-  
-      // Check if the phone number already exists
-      this.checkPhoneExists(phone).then(isPhoneAvailable => {
-        if (!isPhoneAvailable) {
-          // If the phone number exists, update the employee's data
-          const updatedEmployeeData = {
-            name: this.employeeForm.value.name,
-            email: this.employeeForm.value.email,
-            phone: this.employeeForm.value.phone,
-          };
-  
-          const employeeRef = doc(this.firestore, 'Employees', updatedEmployee.employeeId.toString());
-          updateDoc(employeeRef, updatedEmployeeData)
-            .then(() => {
-              console.log('Employee updated in Firestore!');
-              this.fetchEmployees();
-            })
-            .catch((error) => {
-              console.error('Error updating employee in Firestore: ', error);
-            });
-        } else {
-          console.log('Phone number already exists. Data will be updated.');
-        }
-      });
-    }
   }
 
   onSave(): void {
@@ -262,15 +154,47 @@ export class HomeComponent implements OnInit, AfterViewInit  {
             this.fetchEmployees();
             this.employeeForm.reset();
           })
-          .catch((error) => {
-            console.error('Error updating employee in Firestore: ', error);
-          });
-      } else {
-        console.log('No employee found with this phone number.');
-      }
-    }).catch(error => {
-      console.error('Error checking employee phone: ', error);
+      } 
+    })
+  }
+  
+  editEmployee(employeeId: number): void {
+    console.log('Opening dialog to edit employee:', employeeId);
+    const employee = this.employees.find(emp => emp.employeeId === employeeId);
+
+  if (employee) {
+    this.employeeForm.patchValue(employee);
+    
+    const dialogRef = this.dialog.open(this.dialogTemplate!, {
+      width: '2000px',
+      data: employee,  
     });
+
+    dialogRef.afterClosed().subscribe((updatedEmployee: Employee) => {
+      if (updatedEmployee) {
+        this.onSave();
+      }
+    });
+  } 
+  }
+
+  deleteEmployee(employeeId: number): void {
+    const employeesRef = collection(this.firestore, 'Employees');
+    
+    const q = query(employeesRef, where("employeeId", "==", employeeId));
+
+    getDocs(q)
+      .then((querySnapshot) => {
+        querySnapshot.forEach((docSnapshot) => {
+          console.log('Deleting document with ID:', docSnapshot.id);
+          deleteDoc(docSnapshot.ref)
+            .then(() => {
+              console.log('Employee deleted from Firestore');
+              this.fetchEmployees();
+              // alert('Employee deleted successfully!');
+            })
+        });
+      })
   }
   
   get filteredEmployees() {
